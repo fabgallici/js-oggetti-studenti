c1971ea133e215fffca303abf53aa49ed104e84b
cabal82 <fabriziogallici@gmail.com>
2019-10-30 14:52:07 +0100

    es parte 2

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 // aggiungere un nuovo oggetto studente inserendo
 // nell’ordine: nome, cognome e età.
 
+// Prima Parte Esercizio
 var student = {
   "name": 'Marco',
   "surname": 'Rossi',
@@ -15,4 +16,42 @@ var student = {
 
 for (var prop in student) {
   console.log('Dato studente: ' + prop + "  Valore contenuto: " + student[prop]);
+}
+
+
+//Seconda Parte Esercizio
+console.log('--- Inizio Esercizio 2 ---');
+var classroom = [
+  {
+    "name": 'Marco',
+    "surname": 'Rossi',
+    "age": 35
+  },
+  {
+    "name": 'Mario',
+    "surname": 'Bianchi',
+    "age": 18
+  },
+  {
+    "name": 'Paolo',
+    "surname": 'Verdi',
+    "age": 21
+  },
+  {
+    "name": 'Luca',
+    "surname": 'Conti',
+    "age": 29
+  },
+  {
+    "name": 'Luigi',
+    "surname": 'Rizzo',
+    "age": 32
+  }
+];
+
+for (var i = 0; i < classroom.length; i++) {
+  console.log('Dati studente in posizione ' + i);
+  for (var prop in classroom[i]) {
+    console.log('Dato studente: ' + prop + "  Valore contenuto: " + classroom[i][prop]);
+  }
 }
